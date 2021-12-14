34261fc47a2c724edc33b0a0c028b695b6d31bcf
Aakash Bhandari <akashbhandari930@gmail.com>
2021-12-13 18:01:40 -0800

    fixed errors

--- asset/script.js
+++ asset/script.js
@@ -120,7 +120,7 @@ function setScore() {
 
 function getScore() {
     var quizContent = `
-<h2>` + localStorage.getItem("highscoreName") + `'s highscore is:</h2>
+<h2>` + localStorage.getItem("highscoreName") + `'s highscore is: </h2>
 <h1>` + localStorage.getItem("highscore") + `</h1><br> 
 
 <button onclick="clearScore()">Clear score!</button><button onclick="resetGame()">Play Again!</button>
